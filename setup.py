1c972c8eaf6df12f8610a8e2c303e020a4784abe
Danny McDonald <mcddjx@gmail.com>
2019-08-17 14:02:01 +0200

    tabview dep link even though we don't use it!

--- setup.py
+++ setup.py
@@ -31,5 +31,7 @@ setup(
         "dash==1.1.1",
         "dash-daq==0.1.7",
     ],
-    dependency_links=[],
+    dependency_links=[
+        "git+http://github.com/interrogator/tabview.git#egg=tabview-1.4.5"
+    ],
 )
