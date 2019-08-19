92aed5a3912d1483991526f053b13fcb072211ac
Danny McDonald <mcddjx@gmail.com>
2019-08-19 18:45:29 +0200

    newest backend

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license="MIT",
     keywords=[],
     install_requires=[
-        "buzz>=3.0.3",
+        "buzz>=3.0.4",
         "python-dotenv==0.10.3",
         "flask==1.1.1",
         "dash==1.1.1",
