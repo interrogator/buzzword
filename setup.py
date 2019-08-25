5a67636f3946f81c591014c8e22d14b2677edea0
Danny McDonald <mcddjx@gmail.com>
2019-08-25 22:21:51 +0200

    latest buzz

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license="MIT",
     keywords=[],
     install_requires=[
-        "buzz>=3.0.8",
+        "buzz>=3.0.9",
         "python-dotenv==0.10.3",
         "flask==1.1.1",
         "dash==1.1.1",
