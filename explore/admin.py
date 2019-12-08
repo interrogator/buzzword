4282a3f9eda34f863495a1efdce69fff4da6957e
l0rb <lorbritzer@yahoo.de>
2019-12-08 08:00:00 +0100

    activate admin site

--- explore/admin.py
+++ explore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from explore.models import Corpus
 
-# Register your models here.
+
+@admin.register(Corpus)
+class CorpusAdmin(admin.ModelAdmin):
+    list_display = ('slug',)
+
+    def get_fields(self, request, obj):
+        if request.user.is_superuser: 
+            return super().get_fields(request, obj)
+        else: # staff
+            return ['disabled', 'desc', 'name']
+
