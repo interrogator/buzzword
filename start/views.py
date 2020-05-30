5033fc015b1e147445699bf5ac733da571abb2b2
Danny McDonald <mcddjx@gmail.com>
2020-05-30 20:26:40 +0200

    argh tabs

--- start/views.py
+++ start/views.py
@@ -5,19 +5,19 @@ import explore.models
 
 
 def _get_markdown_content(slug, page):
-	"""
-	Load a markdown file
-	"""
-	mdfile = os.path.join("static", slug, page + ".md")
-	with open(mdfile, "r") as fo:
-		data = fo.read()
-	return data
+    """
+    Load a markdown file
+    """
+    mdfile = os.path.join("static", slug, page + ".md")
+    with open(mdfile, "r") as fo:
+        data = fo.read()
+    return data
 
 
 def start(request):
-	"""
-	Load the list-of-corpora style mainpage
-	"""
+    """
+    Load the list-of-corpora style mainpage
+    """
     navbar = request.GET.get("navbar", "home")
     corpora = explore.models.Corpus.objects.filter(disabled=False, load=True)
     context = {"corpora": corpora, "navbar": navbar}
@@ -25,9 +25,9 @@ def start(request):
 
 
 def start_specific(request, slug):
-	"""
-	Load a unique website for this particular slug
-	"""
+    """
+    Load a unique website for this particular slug
+    """
     navbar = request.GET.get("navbar", "home")
     corpus = explore.models.Corpus.objects.get(slug=slug)
     content = _get_markdown_content(slug, navbar)
