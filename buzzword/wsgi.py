fdbce3550fac1c7bff2f91cc702da1c76d520ef1
Danny McDonald <mcddjx@gmail.com>
2020-07-06 20:15:17 +0200

    move imports beside calls

--- buzzword/wsgi.py
+++ buzzword/wsgi.py
@@ -7,18 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+# importing is in this crazy order as a hack.
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buzzword.settings")
 
 from django.conf import settings
 settings.configure()
+
 import django
 django.setup()
 
 import explorer.parts.main
+explorer.parts.main.load_corpora()
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buzzword.settings")
-
-explorer.parts.main.load_corpora()
 application = get_wsgi_application()
