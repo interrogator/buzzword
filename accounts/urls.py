af100c1ae5f4fa9cc1519603ec7bebea82a3c9b1
interrogator <mcddjx@gmail.com>
2020-07-19 16:14:11 +0200

    more migrate handlers

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from accounts import views
-from start import views as start_views
+from buzzword.utils import management_handling
 
-urlpatterns = [
-    path('signup/', start_views.signup, name='signup'),
-    #path("signup/", views.signup, name="signup"),
-    path("logout/", views.logout_view, name="logout"),
-    path("login/", views.login_view, name="login"),
-    path("corpus_settings/", views.corpus_settings, name="corpus_settings"),
+app_name = "accounts"
+
+if management_handling():
+    urlpatterns = []
+else:
+    from start import views as start_views
+    urlpatterns = [
+        path('signup/', start_views.signup, name='signup'),
+        #path("signup/", views.signup, name="signup"),
+        path("logout/", views.logout_view, name="logout"),
+        path("login/", views.login_view, name="login"),
+        path("corpus_settings/", views.corpus_settings, name="corpus_settings"),
 ]
