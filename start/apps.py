d8bacc2b33063eacc1718526a508a7756fdce42a
interrogator <mcddjx@gmail.com>
2020-07-22 13:04:04 +0200

    need languages

--- start/apps.py
+++ start/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from explorer.main import _get_or_load_corpora
+from explorer.main import _get_or_load_corpora, _load_languages
 from buzzword.utils import management_handling
 
 class StartConfig(AppConfig):
@@ -10,8 +10,10 @@ class StartConfig(AppConfig):
         # We load them here to avoid multiple instantiation across other
         # modules, that would take too much time.
         if not management_handling():
-            print("Loading corpora in AppConfig"),
             global corpora, initial_tables
+            print("Loading languages in AppConfig")
+            _load_languages()
+            print("Loading corpora in AppConfig")
             corpora, initial_tables = _get_or_load_corpora()
             print("AppConfig loaded")
         else:
