afeca0556f391ca0a2f94564eb7bf907816d3b1a
interrogator <mcddjx@gmail.com>
2020-08-01 12:36:40 +0200

    non-specific loads again

--- example/views.py
+++ example/views.py
@@ -1,14 +1,18 @@
 from django.conf import settings
 from django.shortcuts import render
-from .app import app
 from explore.models import Corpus
 
 
 def example(request, slug=None):
     specific_nav = False
     if slug is None:
+        from .app import app
         slug = settings.BUZZWORD_SPECIFIC_CORPUS
         specific_nav = True
-    corpus = Corpus.objects.get(slug=slug)
-    context = {"specific_nav": specific_nav, "navbar": "example", "corpus": corpus}
-    return render(request, "example/example.html", context)
+        corpus = Corpus.objects.get(slug=slug)
+        context = {"specific_nav": specific_nav, "navbar": "example", "corpus": corpus}
+        return render(request, f"example/{slug}.html", context)
+    else:
+        context = {"specific_nav": False, "navbar": "example"}
+        return render(request, f"example/example.html", context)
+
