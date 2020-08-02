b7be043350608bd0207f41b3d0fab6a68668ad61
l0rb <lorbritzer@yahoo.de>
2020-08-02 08:00:00 +0200

    improve deployment: branch no longer fixed

--- Dockerfile
+++ Dockerfile
@@ -13,18 +13,26 @@ RUN \
     pip install -r requirements.txt && \
     python setup.py install
 
+ARG BRANCH=master
+
 RUN \
     git clone https://github.com/interrogator/buzzword.git && \
     cd buzzword && \
     git pull --all && \
-    git checkout swisslaw && \
+    git checkout $BRANCH && \
     pip install -r requirements.txt
 
 WORKDIR /buzzword
 
-RUN \
-    cp configs/swiss-law.json corpora.json && \
-    echo "BUZZWORD_SPECIFIC_CORPUS = 'swiss-law'" >> buzzword/settings.py
+ARG CORPUS
+ARG CORPUS_FILE
+
+RUN if [ -n "$CORPUS" ] ; then echo "BUZZWORD_SPECIFIC_CORPUS = '$CORPUS'" >> buzzword/settings.py ; fi
+RUN if [ -n "$CORPUS_FILE" ] ; then cp $CORPUS_FILE corpora.json ; else cp corpora.json.example corpora.json ; fi
+
+#RUN \
+#    cp configs/swiss-law.json corpora.json && \
+#    echo "BUZZWORD_SPECIFIC_CORPUS = 'swiss-law'" >> buzzword/settings.py
 
 RUN chown www-data:www-data . && \
     chmod 777 . && \
---
+++ deploy.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+# deployment script to run on server for buzzword swisslaw
+
+# Run it like this. Container name will be buzzword_[BRANCH_NAME]
+# ./configs/swiss-law-deploy.sh [DJANGO PASSWORD] [BRANCH_NAME]
+
+# Parse command-line options
+
+# Option strings
+SHORT=b:d:c:
+LONG=branch:,django-password:,corpus:,corpus-file:,no-cache,cwdmount,port:
+
+# read the options
+OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+
+if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+# set initial values
+BRANCH="master"
+DJANGO_SUPERUSER_PASSWORD=""
+CORPUS=""
+CORPUS_FILE=""
+NOCACHE=""
+CWDMOUNT=false
+PORT=80
+
+while true ; do
+  case "$1" in
+    -b | --branch )
+      BRANCH="$2"
+      shift 2
+      ;;
+    -d | --django-password )
+      DJANGO_SUPERUSER_PASSWORD="$2"
+      shift 2
+      ;;
+    -c | --corpus )
+      CORPUS="--build-arg CORPUS=$2"
+      shift 2
+      ;;
+    --corpus-file )
+      CORPUS_FILE="--build-arg CORPUS_FILE=$2"
+      shift 2
+      ;;
+    --port )
+      PORT=$2
+      shift 2
+      ;;
+    --no-cache )
+      NOCACHE="--no-cache"
+      shift
+      ;;
+    --cwdmount )
+      CWDMOUNT=true
+      shift
+      ;;
+    -- )
+      shift
+      break
+      ;;
+  esac
+done
+
+NAME="buzzword_$BRANCH"
+TAG="buzzword:$BRANCH"
+
+if [ -z "$(git show-ref refs/heads/$BRANCH)" ]; then
+    echo "Branch $BRANCH does not exist."
+    exit 1
+fi
+
+# get latest repo
+#git checkout $BRANCH
+git pull
+
+# build the image (note, data is still missing)
+BUILD_BRANCH="--build-arg BRANCH=$BRANCH"
+sudo docker build - < Dockerfile $CORPUS $CORPUS_FILE $BUILD_BRANCH $NOCACHE -t $TAG
+
+# now kill and delete the old container if running
+sudo docker stop $NAME
+sudo docker container rm $NAME
+
+touch db.sqlite3
+
+#if $CWDMOUNT ; then
+
+# add settings, db and data in as volume
+MOUNT="
+    --mount type=bind,source="$(pwd)"/buzzword/settings.py,target=/buzzword/buzzword/settings.py \
+    --mount type=bind,source="$(pwd)"/db.sqlite3,target=/buzzword/db.sqlite3
+    --mount type=bind,source="$(pwd)"/static/corpora,target=/buzzword/static/corpora
+"
+
+runcmd="sudo docker run -itd -p $PORT:8000
+    $MOUNT
+    -e \"DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD\"
+    --name $NAME
+    $TAG
+    "
+$runcmd
+
+# all the commands we need to do to get configured
+sudo docker exec -it $NAME python manage.py migrate
+sudo docker exec -it $NAME python manage.py load_languages
+sudo docker exec -it $NAME python manage.py load_corpora
+sudo docker exec -it $NAME python manage.py do_ocr
+if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    echo "Creating superusers with password: $DJANGO_SUPERUSER_PASSWORD"
+    sudo docker exec -it $NAME python manage.py createsuperuser --noinput --username danny --email danny@testing.com
+    sudo docker exec -it $NAME python manage.py createsuperuser --noinput --username martin.kurz --email martin@testing.com
+fi
+
+echo "Container name: $NAME"
+
+
