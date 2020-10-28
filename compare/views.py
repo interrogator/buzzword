06bc7c0a94d0255e17db3986506447ebeedd37b2
Danny <mcddjx@gmail.com>
2020-10-28 14:37:37 +0100

    search feature prototype

--- compare/views.py
+++ compare/views.py
@@ -12,10 +12,27 @@ from django.contrib.auth.models import User
 
 from .forms import PostForm, SubmitForm
 from .models import PDF, OCRUpdate
-from .utils import markdown_to_buzz_input, store_buzz_raw
+from .utils import markdown_to_buzz_input, store_buzz_raw, _get_sections
 
 from buzzword.utils import _make_message
 from explore.models import Corpus
+
+
+
+def _latest_ocrupdate(pdf, username):
+    ocrs = OCRUpdate.objects.filter(pdf=pdf)
+    # of this pdf, get the latest ocr update submitted by this user
+    user_latest = ocrs.filter(username=username)
+    if user_latest:
+        user_latest = user_latest.latest("timestamp")
+    # also get the latest ACCEPTED ocr
+    regular_latest = ocrs.filter(accepted=True).latest("timestamp")
+    # if there is a user latest, and it's newer than the latest accepted, show that
+    if user_latest and user_latest.timestamp >= regular_latest.timestamp:
+        return user_latest
+    # otherwise, show the latest accepted text
+    else:
+        return regular_latest
 
 
 @login_required
@@ -39,28 +56,21 @@ def browse_collection(request, slug=None):
         page_number = int(query)
     else:
         page_number = int(request.GET.get("page", 1))
+    sections = _get_sections(slug)
+    text_search = request.GET.get("search")
+    search_results = None
+    if text_search:
+        search_results = _text_search(text_search, slug, request.user.username, sections)
     page_obj = paginator.get_page(page_number)
     pdf = all_pdfs.get(slug=slug, num=page_number - 1)
     pdf_path = os.path.relpath(pdf.path)
     template = loader.get_template("compare/sidetoside.html")
     # get all the updates for this particular pdf
-    this_pdf = OCRUpdate.objects.filter(pdf=pdf)
-    # of these, get the latest ocr update submitted by this user
-    user_latest = this_pdf.filter(username=request.user.username)
-    if user_latest:
-        user_latest = user_latest.latest("timestamp")
-    # also get the latest ACCEPTED ocr
-    regular_latest = this_pdf.filter(accepted=True).latest("timestamp")
-    # if there is a user latest, and it's newer than the latest accepted, show that
-    if user_latest and user_latest.timestamp >= regular_latest.timestamp:
-        plaintext = user_latest.text
-    # otherwise, show the latest accepted text
-    else:
-        plaintext = regular_latest.text
-
+    plaintext = _latest_ocrupdate(pdf, username=request.user.username).text
     default_commit = f"Update {os.path.splitext(os.path.basename(pdf_path))[0]}"
     form_data = {"description": plaintext, "commit_msg": default_commit}
     form = PostForm(initial=form_data)
+    
     context = {
         "pdf_filepath": "/" + pdf_path.replace(".tif", ".pdf"),
         "form": form,
@@ -68,6 +78,11 @@ def browse_collection(request, slug=None):
         "specific_nav": bool(settings.BUZZWORD_SPECIFIC_CORPUS),
         "corpus": Corpus.objects.get(slug=slug),
         "navbar": "compare",
+        "text_search": search_results,
+        "sections": sections,
+        "colwidth": "48vw" if not sections else "40vw",
+        "coldata": "-5" if sections else "",
+        "search_query": text_search
     }
     # if the user has tried to update the OCR text
     if request.method == "POST":
@@ -120,3 +135,59 @@ def browse_collection(request, slug=None):
                 _make_message(request, messages.WARNING, msg)
 
     return HttpResponse(template.render(context, request))
+
+
+def _get_book_and_chapter(pdf, sections):
+    for book, info in sections.items():
+        for chapter, subinfo in info["chapters"].items():
+            # todo: inclusive exclusive?
+            if subinfo["start"] <= pdf.num <= subinfo["end"]:
+                return book, chapter
+    for book, info in sections.items():
+        if pdf.num >= info["start"] <= info["end"]:
+            return book, ""
+    return "", ""
+
+
+def _text_search(query, slug, username, sections):
+    """
+    get or create a search, returning the resulting queryset
+
+    todo: store user's latest 'update' and 
+    """
+    # exists = TextSearches.objects.get(corpus=corpus, query=query)
+    corpus = Corpus.objects.get(slug=slug)
+    exists = False
+    has_not_updated = False  # todo
+    if exists and has_not_updated:
+        all_pdfs = exists.pdfs_set.all()
+        results = [(pdf, _latest_ocrupdate(pdf, username=username).text) for pdf in all_pdfs]
+    else:
+        all_pdfs = PDF.objects.filter(slug=slug)
+        results = []
+        for pdf in all_pdfs:
+            plaintext = _latest_ocrupdate(pdf, username=username).text
+            occurrences = plaintext.count(query)
+            if not occurrences:
+                continue
+            for i in range(occurrences):
+                index = plaintext.index(query)            
+                title = ""
+                book, chapter = _get_book_and_chapter(pdf, sections)
+                page = pdf.num+1
+                cut_text = plaintext[max(0, index-120):index+120].replace("\n", " ")
+                left, right = cut_text.split(query, 1)
+                line = dict(
+                    left=left,
+                    match=query,
+                    right=right,
+                    title=title,
+                    page=page,
+                    book=book,
+                    chapter=chapter
+                )
+                results.append(line)
+                plaintext = plaintext.split(query, 1)[-1]
+    return results
+    #paginator = Paginator(all_pdfs, 10)
+    #return paginator.get_page(0)
