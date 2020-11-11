fd9488f5189b9566f2a7efc53594313557516db2
Danny <mcddjx@gmail.com>
2020-11-11 15:19:51 +0100

    bootstrap update input field

--- compare/forms.py
+++ compare/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 
 from .models import Post
-
+from django.forms.widgets import TextInput
 
 class SubmitForm(forms.Form):
     description = forms.CharField()
-    commit_msg = forms.CharField(widget=forms.TextInput())
+    commit_msg = forms.CharField()
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = "__all__"
+
+    commit_msg = forms.CharField(widget=TextInput(attrs={'class': 'form-control'}))
